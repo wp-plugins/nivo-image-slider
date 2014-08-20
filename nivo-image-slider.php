e3f02a8b6958dc7f139cea5213e21efd1b8a43a5
sayful <sayful@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-08-20 16:41:47 +0000

    Adding new version 1.2 of the plugin
    git-svn-id: https://plugins.svn.wordpress.org/nivo-image-slider/trunk@969100 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- nivo-image-slider.php
+++ nivo-image-slider.php
@@ -1,9 +1,9 @@
 <?php
 /*
  * Plugin Name: Nivo Image Slider
- * Plugin URI: http://sayful.net/
+ * Plugin URI: http://wordpress.org/plugins/nivo-image-slider/
  * Description: A WordPress plugin to include image slider into your theme.
- * Version: 1.1
+ * Version: 1.2
  * Author: Sayful Islam
  * Author URI: http://www.sayful.net
  * Text Domain: nivoslider
@@ -19,7 +19,7 @@ function sis_nivoslider_load_textdomain() {
 }
 add_action( 'plugins_loaded', 'sis_nivoslider_load_textdomain' );
 
-/* Adding Latest jQuery from Wordpress plugin */
+/* Adding Latest jQuery for Wordpress plugin */
 function sis_nivoslider_plugin_scripts() {
 	wp_enqueue_script('jquery');
 	wp_enqueue_script('sis_nivoslider_script',plugins_url( '/js//jquery.nivo.slider.js' , __FILE__ ),array( 'jquery' ));
@@ -91,48 +91,42 @@ function nivoslider_custom_post_type() {
 add_action( 'init', 'nivoslider_custom_post_type', 0 );
 
 
-/* Slider Loop */
-function sis_get_nivoslider(){
-	$slider= '<div class="slider-wrapper theme-default"><div id="slider" class="nivoSlider">';
+
+/**add the shortcode for the slider- for use in editor**/
+function sis_insert_nivoslider($atts, $content=null){
+    extract(shortcode_atts(array(
+        'theme' =>'default',
+    ), $atts));  
+
+	$slider= '<div class="slider-wrapper theme-'.$theme.'"><div id="slider" class="nivoSlider">';
 	$efs_query= "post_type=slider&posts_per_page=-1";
 	query_posts($efs_query);
-	if (have_posts()) : while (have_posts()) : the_post(); 
+	if (have_posts()) : while (have_posts()) : the_post();
+
 		$slider_image = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'full' );	
 		$slider.='<img src="'.$slider_image[0].'" data-thumb="'.$slider_image[0].'" alt="" title="'.get_the_title().'" />';		
 	endwhile; endif; wp_reset_query();
 	$slider.= '</div></div>';
 	return $slider;
 }
-
-/**add the shortcode for the slider- for use in editor**/
-function sis_insert_nivoslider($atts, $content=null){
-	$slider= sis_get_nivoslider();
-	return $slider;
-}
 add_shortcode('all-nivoslider', 'sis_insert_nivoslider');
 
 /* Slider for individual image */
 function sis_slider_wrapper_shortcode( $atts, $content = null ) {
-        extract(shortcode_atts(array(
-                        'theme' =>'default',
-                        'id' =>'sisnivoslider',
-                ), $atts));    
-        return '<div class="slider-wrapper theme-'.$theme.'">
-        	<div id="'.$id.'" class="nivoSlider">'.do_shortcode($content).'</div></div>
-        	<script type="text/javascript">
-	    		jQuery(window).load(function() {
-	        		jQuery("#'.$id.'").nivoSlider();
-	    		});
-    		</script>';
+    extract(shortcode_atts(array(
+        'theme' =>'default',
+        'id' =>'sisnivoslider',
+    ), $atts));    
+    return '<div class="slider-wrapper theme-'.$theme.'"><div id="'.$id.'" class="nivoSlider">'.do_shortcode($content).'</div></div><script type="text/javascript">jQuery(window).load(function(){jQuery("#'.$id.'").nivoSlider()});</script>';
 }
 add_shortcode( 'nivo-slider', 'sis_slider_wrapper_shortcode' );
  
 function sis_slide_shortcode( $atts, $content = null ) {
-        extract(shortcode_atts(array(
-                        'image_link' =>'',
-                        'caption' =>'',
-                        'alt' =>'',
-                ), $atts));    
-        return '<img src="'.$image_link.'" data-thumb="'.$image_link.'" alt="'.$alt.'" title="'.$caption.'" />';
+    extract(shortcode_atts(array(
+        'image_link' =>'',
+        'caption' =>'',
+        'alt' =>'',
+    ), $atts));  
+    return '<img src="'.$image_link.'" data-thumb="'.$image_link.'" alt="'.$alt.'" title="'.$caption.'" />';
 }
 add_shortcode( 'nivoslides', 'sis_slide_shortcode' );
