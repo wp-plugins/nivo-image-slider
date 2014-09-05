4a0a4a4b56373602b3d8a0599b0279a5cd53cb80
sayful <sayful@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-09-05 18:21:09 +0000

    tagging version 1.2
    git-svn-id: https://plugins.svn.wordpress.org/nivo-image-slider/tags/1.2@982176 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ trunk/nivo-image-slider.php
@@ -0,0 +1,132 @@
+<?php
+/*
+ * Plugin Name: Nivo Image Slider
+ * Plugin URI: http://wordpress.org/plugins/nivo-image-slider/
+ * Description: A WordPress plugin to include image slider into your theme.
+ * Version: 1.2
+ * Author: Sayful Islam
+ * Author URI: http://www.sayful.net
+ * Text Domain: nivoslider
+ * Domain Path: /languages/
+ * License: GPL2
+*/
+
+/**
+ * Load plugin textdomain.
+ */
+function sis_nivoslider_load_textdomain() {
+  load_plugin_textdomain( 'nivoslider', false, dirname( plugin_basename( __FILE__ ) ) . '/languages/' );
+}
+add_action( 'plugins_loaded', 'sis_nivoslider_load_textdomain' );
+
+/* Adding Latest jQuery for Wordpress plugin */
+function sis_nivoslider_plugin_scripts() {
+	wp_enqueue_script('jquery');
+	wp_enqueue_script('sis_nivoslider_script',plugins_url( '/js/jquery.nivo.slider.js' , __FILE__ ),array( 'jquery' ));
+
+	wp_enqueue_style('sis_nivoslider_style',plugins_url( '/css/nivo-slider.css' , __FILE__ ));
+	//Nivo Theme
+	wp_enqueue_style('sis_nivoslider_default',plugins_url( '/themes/default/default.css' , __FILE__ ));
+	wp_enqueue_style('sis_nivoslider_light',plugins_url( '/themes/light/light.css' , __FILE__ ));
+	wp_enqueue_style('sis_nivoslider_dark',plugins_url( '/themes/dark/dark.css' , __FILE__ ));
+	wp_enqueue_style('sis_nivoslider_bar',plugins_url( '/themes/bar/bar.css' , __FILE__ ));
+}
+add_action('init', 'sis_nivoslider_plugin_scripts');
+
+
+function sis_nivoslider_activation(){
+	?>
+    <script type="text/javascript">
+    	jQuery(window).load(function() {
+        	jQuery('#slider').nivoSlider();
+    	});
+    </script>
+	<?php
+}
+add_action('wp_footer','sis_nivoslider_activation');
+
+// Register Custom Post Type for Slider
+function nivoslider_custom_post_type() {
+
+	$labels = array(
+		'name'                => _x( 'Slides', 'Post Type General Name', 'nivoslider' ),
+		'singular_name'       => _x( 'Slide', 'Post Type Singular Name', 'nivoslider' ),
+		'menu_name'           => __( 'Slider', 'nivoslider' ),
+		'parent_item_colon'   => __( 'Parent Slide:', 'nivoslider' ),
+		'all_items'           => __( 'All Slides', 'nivoslider' ),
+		'view_item'           => __( 'View Slide', 'nivoslider' ),
+		'add_new_item'        => __( 'Add New Slide', 'nivoslider' ),
+		'add_new'             => __( 'Add New', 'nivoslider' ),
+		'edit_item'           => __( 'Edit Slide', 'nivoslider' ),
+		'update_item'         => __( 'Update Slide', 'nivoslider' ),
+		'search_items'        => __( 'Search Slide', 'nivoslider' ),
+		'not_found'           => __( 'Not found', 'nivoslider' ),
+		'not_found_in_trash'  => __( 'Not found in Trash', 'nivoslider' ),
+	);
+	$args = array(
+		'label'               => __( 'slider', 'nivoslider' ),
+		'description'         => __( 'Post Type Description', 'nivoslider' ),
+		'labels'              => $labels,
+		'supports'            => array( 'title', 'editor', 'thumbnail', ),
+		'hierarchical'        => false,
+		'public'              => true,
+		'show_ui'             => true,
+		'show_in_menu'        => true,
+		'show_in_nav_menus'   => true,
+		'show_in_admin_bar'   => true,
+		'menu_position'       => 5,
+		'menu_icon'           => 'dashicons-slides',
+		'can_export'          => true,
+		'has_archive'         => true,
+		'exclude_from_search' => false,
+		'publicly_queryable'  => true,
+		'rewrite'             => array('slug' => 'slide',),
+		'capability_type'     => 'page',
+	);
+	register_post_type( 'slider', $args );
+
+}
+
+// Hook into the 'init' action
+add_action( 'init', 'nivoslider_custom_post_type', 0 );
+
+
+
+/**add the shortcode for the slider- for use in editor**/
+function sis_insert_nivoslider($atts, $content=null){
+    extract(shortcode_atts(array(
+        'theme' =>'default',
+    ), $atts));  
+
+	$slider= '<div class="slider-wrapper theme-'.$theme.'"><div id="slider" class="nivoSlider">';
+	$efs_query= "post_type=slider&posts_per_page=-1";
+	query_posts($efs_query);
+	if (have_posts()) : while (have_posts()) : the_post();
+
+		$slider_image = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'full' );	
+		$slider.='<img src="'.$slider_image[0].'" data-thumb="'.$slider_image[0].'" alt="" title="'.get_the_title().'" />';		
+	endwhile; endif; wp_reset_query();
+	$slider.= '</div></div>';
+	return $slider;
+}
+add_shortcode('all-nivoslider', 'sis_insert_nivoslider');
+
+/* Slider for individual image */
+function sis_slider_wrapper_shortcode( $atts, $content = null ) {
+    extract(shortcode_atts(array(
+        'theme' =>'default',
+        'id' =>'sisnivoslider',
+    ), $atts));    
+    return '<div class="slider-wrapper theme-'.$theme.'"><div id="'.$id.'" class="nivoSlider">'.do_shortcode($content).'</div></div><script type="text/javascript">jQuery(window).load(function(){jQuery("#'.$id.'").nivoSlider()});</script>';
+}
+add_shortcode( 'nivo-slider', 'sis_slider_wrapper_shortcode' );
+ 
+function sis_slide_shortcode( $atts, $content = null ) {
+    extract(shortcode_atts(array(
+        'image_link' =>'',
+        'caption' =>'',
+        'alt' =>'',
+    ), $atts));  
+    return '<img src="'.$image_link.'" data-thumb="'.$image_link.'" alt="'.$alt.'" title="'.$caption.'" />';
+}
+add_shortcode( 'nivoslides', 'sis_slide_shortcode' );
---
+++ trunk/readme.txt
@@ -0,0 +1,117 @@
+=== Nivo Image Slider ===
+Contributors: sayful
+Tags: slider, image slider, nivo slider, images, wordpress slider, jquery slider, responsive
+Requires at least: 3.0
+Tested up to: 4.0
+Stable tag: 1.2
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+A WordPress Image slider based on Nivo Slider to display image slider on post, page or theme.
+
+== Description ==
+
+A WordPress Image slider based on [Nivo Slider](http://dev7studios.com/plugins/nivo-slider/) to display image slider on post, page or theme. It's an easy, simple & responsive slider, which looks awesome in every single theme.
+
+Just create your slides from the Slider menu & paste the following shortcode where you want to display this slider:
+
+`[all-nivoslider theme='default|dark|light|bar']`
+
+For example: if you want to add 'light' theme just write as following code
+
+`[all-nivoslider theme='light']`
+
+Or you can paste following to add slider to your theme:
+
+`<?php echo do_shortcode('[all-nivoslider theme='light']'); ?>`
+
+If you want to use multiple slider at diffrent page or post at your theme write the following code
+
+`[nivo-slider][nivoslides image_link=''][/nivo-slider]`
+
+Inside [nivo-slider], you can add id="" to make it unique and you can add theme by adding theme="" and the value of theme that you can use is 'default', 'dark', 'light' or 'bar' as following
+
+`[nivo-slider id='unique-1' theme='default'][/nivo-slider]`
+`[nivo-slider id='unique-2' theme='dark'][/nivo-slider]`
+`[nivo-slider id='unique-3' theme='light'][/nivo-slider]`
+`[nivo-slider id='unique-4' theme='bar'][/nivo-slider]`
+
+Repeat "[nivoslides image_link='']" as many image as you want. Inside image_link='' put you image link like "http://lorempixel.com/400/200/sports/1/"
+
+Inside [nivoslides image_link=''], you can also add 
+
+alt="" to add alternative text,
+
+caption="" to add caption to slide like this
+
+`[nivoslides image_link='' alt='' caption='']`
+
+Here is a complete example of Nivo Slider with ten image and dark theme.
+
+`[nivo-slider id='unique-2' theme='dark'][nivoslides image_link='http://lorempixel.com/400/200/sports/1' caption='This is caption one'][nivoslides image_link='http://lorempixel.com/400/200/sports/2' caption='This is caption two'][nivoslides image_link='http://lorempixel.com/400/200/sports/3' caption='This is caption three'][nivoslides image_link='http://lorempixel.com/400/200/sports/4' caption='This is caption four'][nivoslides image_link='http://lorempixel.com/400/200/sports/5' caption='This is caption five'][nivoslides image_link='http://lorempixel.com/400/200/sports/6' caption='This is caption six'][nivoslides image_link='http://lorempixel.com/400/200/sports/7' caption='This is caption seven'][nivoslides image_link='http://lorempixel.com/400/200/sports/8' caption='This is caption eight'][/nivo-slider]`
+
+
+
+<h1>Features</h1>
+
+1. Fully Responsive 
+2. Unlimited Slides
+3. Supported in all major browsers
+4. Beautiful Transition Effects
+5. Custom Post and Shortcode system
+
+== Installation ==
+
+Installing the plugins is just like installing other WordPress plugins. If you don't know how to install plugins, please review the two options below:
+
+Install by Search
+
+* From your WordPress dashboard, choose 'Add New' under the 'Plugins' category.
+* Search for 'Nivo Image Slider' a plugin will come called 'Nivo Image Slider by Sayful Islam' and Click 'Install Now' and confirm your installation by clicking 'ok'
+* The plugin will download and install. Just click 'Activate Plugin' to activate it.
+
+Install by ZIP File
+
+* From your WordPress dashboard, choose 'Add New' under the 'Plugins' category.
+* Select 'Upload' from the set of links at the top of the page (the second link)
+* From here, browse for the zip file included in your plugin titled 'nivo-image-slider.zip' and click the 'Install Now' button
+* Once installation is complete, activate the plugin to enable its features.
+
+Install by FTP
+
+* Find the directory titles 'nivo-image-slider' and upload it and all files within to the plugins directory of your WordPress install (WORDPRESS-DIRECTORY/wp-content/plugins/) [e.g. www.yourdomain.com/wp-content/plugins/]
+* From your WordPress dashboard, choose 'Installed Plugins' option under the 'Plugins' category
+* Locate the newly added plugin and click on the 'Activate' link to enable its features.
+
+
+== Frequently Asked Questions ==
+Do you have questions or issues with Nivo Images Slider? [Ask for support here.](http://wordpress.org/support/plugin/nivo-image-slider)
+
+== Screenshots ==
+
+1. Screenshot of Nivo Images Slider Shortcode.
+2. Screenshot of Nivo Images Slider on post.
+
+== Changelog ==
+
+= version 1.2 =
+* A new option has been added to add theme for Slider from custom post type.
+
+= version 1.1 =
+* To make the plugin translation ready.
+* The plugin is transtated to Bengali Language.
+
+= version 1.0 =
+* Implementation of basic functionality.
+
+== CREDIT ==
+
+1.This plugin was developed by [Sayful Islam](http://sayful.net)
+
+== CONTACT ==
+
+[Sayful Islam](http://sayful1.wordpress.com/100-2/)
+
+== Upgrade Notice ==
+
+Upgrade the plugin to get latest feature and faster speed.
