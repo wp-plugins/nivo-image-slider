7981608373725dc37ba6fd8a2bb7bb60621e4969
sayful <sayful@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-02-21 07:57:33 +0000

    Adding new version
    git-svn-id: https://plugins.svn.wordpress.org/nivo-image-slider/trunk@1095748 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- nivo-image-slider.php
+++ nivo-image-slider.php
@@ -9,7 +9,7 @@
  * Author URI:        http://sayful.net/
  * License:           GPL-2.0+
  * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
- * Text Domain:       nivoslider
+ * Text Domain:       nivo-image-slider
  * Domain Path:       /languages/
  */
 
